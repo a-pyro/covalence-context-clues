370b4db2d5ba4c3fbd579348b4475b59bc4a21f4
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2020-11-11 19:08:51 +0100

    added h3's

---
+++ script.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // console.log('dom loaded');
+    var friends = ['Mariottide', 'Gerry', 'Pasquale', 'Osvaldo', 'Ermenegildo'];
+    var rooms = [
+        'living',
+        'bath',
+        'computer',
+        'fart',
+        'burp',
+        'dreaming',
+        'smoking',
+        'screaming',
+        'self-love',
+        'sex',
+    ];
+
+    var weapons = [
+        'tampax',
+        'rubber duck',
+        'underwear',
+        'toblerone',
+        'c++',
+        'keyboard',
+        'sock',
+        'carillon',
+        'story of his life',
+        'picture of his wife',
+        'smell of his armpit',
+        'vegan diet',
+        'sound of his music',
+        'selfie he made that morning',
+        'bla bla bla',
+        'karate move',
+        'chuck norris name invocation',
+        'smell of his fart',
+        'chore of having to choose 20 different weapon names',
+        'watergun',
+    ];
+    // load 100 h3
+    for (var i = 0; i < 100; i++) {
+        var heading = document.createElement('h3');
+        heading.classList.add(
+            'animate__animated',
+            `${i % 2 === 0 ? 'animate__backInLeft' : 'animate__backInRight'}`
+        );
+        heading.innerText = `Accusation ${i + 1} `;
+        heading.id = `${i + 1}`;
+        document.querySelector('.container').appendChild(heading);
+
+        // taking only indexes from 0 to 4
+        var friend = i % 10 > 4 ? friends[(i - 5) % 10] : friends[i % 10];
+        // heading.innerText = friend;
+        var weapon;
+        var room;
+
+        heading.addEventListener('click', letsTryClosures());
+    }
+    function letsTryClosures() {
+        return function closure(e) {
+            console.log(e.currentTarget, friend);
+            // console.log(e.currentTarget);
+        };
+    }
+});
