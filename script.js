31fd6ac3e9645a64f08b8e773011200af75d320b
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2020-11-12 12:52:34 +0100

    lab finished

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
     ];
 
     var weapons = [
-        'tampax',
+        'lightsaber',
         'rubber duck',
         'underwear',
         'toblerone',
@@ -41,27 +41,53 @@ document.addEventListener('DOMContentLoaded', () => {
         var heading = document.createElement('h3');
         heading.classList.add(
             'animate__animated',
-            `${i % 2 === 0 ? 'animate__backInLeft' : 'animate__backInRight'}`
+            `${i % 2 === 0 ? 'animate__backInLeft' : 'animate__backInRight'}`,
+            'my-0'
         );
         heading.innerText = `Accusation ${i} `;
-        heading.id = `${i + 1}`;
-        document.querySelector('.container').appendChild(heading);
 
-        // taking only indexes from 0 to 4
-        var friend = i % 10 > 4 ? friends[(i - 5) % 10] : friends[i % 10];
-        // heading.innerText = friend;
-        var weapon;
-        var room;
+        // heading.id = `${i}`;
+        // heading.setAttribute('data-toggle', 'modal');
+        setAttributes(heading, {
+            id: `${i}`,
+            'data-toggle': 'modal',
+            'data-target': '#staticBackdrop',
+        });
+
+        document.querySelector('.container').appendChild(heading);
 
         heading.addEventListener('click', letsTryClosures(i));
     }
-    // console.log(i);
 
     function letsTryClosures(num) {
-        return function () {
-            // console.log(e.currentTarget, friend);
-            console.log(num);
-            // console.log(e.currentTarget);
+        return function (e) {
+            // taking only indexes from 0 to 4
+            var friend =
+                num % 10 > 4 ? friends[(num - 5) % 10] : friends[num % 10];
+            var weapon = weapons[num % weapons.length];
+            var room = `${rooms[num % rooms.length]} room`;
+            document.querySelector(
+                '.modal-title'
+            ).innerText = `Accusation ${num}`;
+
+            document.querySelector(
+                '.modal-body'
+            ).innerText = `Accusation ${num}: I accuse ${friend}, with the ${weapon} in the ${room}`;
+            // console.log(num, friend, e.currentTarget);
         };
     }
+
+    function setAttributes(element, attributesObject) {
+        Object.keys(attributesObject).forEach((key) =>
+            element.setAttribute(key, attributesObject[key])
+        );
+    }
+
+    // const h3s = document.querySelectorAll('h3');
+
+    // for (var index = 0; index < h3s.length; index++) {
+    //     let ind = index;
+    //     console.log(ind, index);
+    //     h3s[index].addEventListener('click', () => console.log(index));
+    // }
 });
